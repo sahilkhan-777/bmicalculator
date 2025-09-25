813c7df7d45f5f4b2f7c8312570dc8b977cc7566
Sahil Khan <78162192+sahilkhan-777@users.noreply.github.com>
2025-09-25 23:45:36 +0530

    fix: change code from vanilla JS to jQuery

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const heightInput = document.querySelector("#height");
-const weightInput = document.querySelector("#weight");
-const bmi = document.querySelector("#bmi");
-const calculateBtn = document.querySelector("#calculate-button");
-
+const heightInput = $("#height");
+const weightInput = $("#weight");
+const bmi = $("#bmi");
+const bmiText = $("#bmi-text");
 
 let finalBmi = 0;
 
 // debugger;
-calculateBtn.onclick = calculateBMI;
+$("#calculate-button").click(calculateBMI);
 
 function calculateBMI() {
-    let heightValue = parseFloat(heightInput.value) / 100;
-    let weightValue = parseFloat(weightInput.value);
-    // let bmiMessage = "";
-
+    let heightValue = parseFloat(heightInput.val()) / 100;
+    let weightValue = parseFloat(weightInput.val());
+    
     finalBmi = weightValue / (heightValue * heightValue);
     finalBmi = finalBmi.toFixed(2);
-    console.log(finalBmi);
-    bmi.textContent = finalBmi;
+    // console.log(finalBmi);
+    bmiText.text("Your BMI is:");
+    bmi.text(finalBmi);
 }
 
